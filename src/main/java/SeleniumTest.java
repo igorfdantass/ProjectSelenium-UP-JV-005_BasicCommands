8f2b4735610c9d991e03c79b65c44e6703a8fff1
igor.dantas <igorferreiradantas@gmail.com>
2023-03-28 22:38:31 -0300

    #Aula2

--- src/main/java/SeleniumTest.java
+++ src/main/java/SeleniumTest.java
@@ -1,5 +1,7 @@
 public class SeleniumTest {
     public static void main(String[] args) {
-        SeleniumElements test = new SeleniumElements();
+        //SeleniumElements test = new SeleniumElements();
+        //SeleniumFireFoxElements testFireFox = new SeleniumFireFoxElements();
+        SeleniumEdgeElements testEdge = new SeleniumEdgeElements();
     }
 }
