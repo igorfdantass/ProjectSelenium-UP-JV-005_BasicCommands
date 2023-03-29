e29d4e95ecf5cde506bc58fac61783e5c4d5e310
igor.dantas <igorferreiradantas@gmail.com>
2023-03-28 22:17:38 -0300

    #Aula1

---
+++ src/main/java/SeleniumElements.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+public class SeleniumElements {
+    ChromeDriver driver;
+
+//Forma 4 - Hard Coded Location
+//    public SeleniumElements(){
+//        System.setProperty("webdriver.chrome.driver","C:\\Users\\igor.dantas\\Drivers\\bin\\chromedriver.exe");
+//        ChromeDriver driver = new ChromeDriver();
+//    }
+
+//Forma 3 - The Path Enviroment Variable
+//    public SeleniumElements(){
+//        driver = new ChromeDriver();
+//    }
+
+}
---
+++ src/main/java/SeleniumTest.java
@@ -0,0 +1,5 @@
+public class SeleniumTest {
+    public static void main(String[] args) {
+        SeleniumElements test = new SeleniumElements();
+    }
+}
